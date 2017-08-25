4009e0a2285d3f93985aec56d8705833d53ad0e2
Sourav@Beas <beasmac03@Souravs-Mac-mini.local>
2017-08-25 12:16:46 +0530

    initial commit

---
+++ SessionHandler/SessionHandler.h
@@ -0,0 +1,19 @@
+//
+//  SessionHandler.h
+//  SessionHandler
+//
+//  Created by Sourav@Beas on 19/07/17.
+//  Copyright © 2017 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SessionHandler.
+FOUNDATION_EXPORT double SessionHandlerVersionNumber;
+
+//! Project version string for SessionHandler.
+FOUNDATION_EXPORT const unsigned char SessionHandlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SessionHandler/PublicHeader.h>
+
+
---
+++ SessionHandler/SessionHandler.swift
@@ -0,0 +1,77 @@
+//
+//  SessionHandler.swift
+//  HRIS
+//
+//  Created by Sourav@Beas on 24/03/17.
+//  Copyright © 2017 BEAS. All rights reserved.
+//
+
+import Foundation
+
+protocol SessionHandlerDelegate:class {
+    func WSError(errorMessage:String)
+    func WSSuccess(response:Any?,webserviceType:String?)
+}
+
+class SessionHandler:NSObject{
+    fileprivate let sessionConfigaration = URLSessionConfiguration.default
+    fileprivate var urlSession:URLSession? = nil
+    public weak var sessionDelegate:SessionHandlerDelegate?
+    public var identifier:String? = nil
+    override init() {
+        self.urlSession = URLSession(configuration: sessionConfigaration)
+    }
+    public func callWebservice(baseUrl:URL,httpMethod:String,acceptType:String,contentType:String,httpBody:Data?,authentication:String?) throws {
+        var urlRequest = URLRequest(url: baseUrl, cachePolicy: .reloadIgnoringCacheData, timeoutInterval: 60)
+        urlRequest.httpMethod = httpMethod
+        if let httpAuthentication = authentication {
+            urlRequest.setValue(httpAuthentication, forHTTPHeaderField: "Authorization")
+        }
+        urlRequest.setValue(acceptType, forHTTPHeaderField: "Accept")
+        urlRequest.setValue(contentType, forHTTPHeaderField: "Content-Type")
+        if let body = httpBody {
+            urlRequest.httpBody = body
+        }
+        let dataTask = urlSession?.dataTask(with: urlRequest, completionHandler: { (data, reseponse, error) in
+            
+            if (error == nil) {
+                 let httpResponse = reseponse as! HTTPURLResponse
+                if httpResponse.statusCode == 200 {
+                    if (self.sessionDelegate != nil ){
+                        OperationQueue.main.addOperation({
+                            
+                            self.sessionDelegate?.WSSuccess(response: data,webserviceType: self.identifier)
+                        })
+                    }
+                    
+                }else{
+                    if (self.sessionDelegate != nil){
+                        OperationQueue.main.addOperation({
+                            if (reseponse != nil) {
+                                self.sessionDelegate?.WSError(errorMessage:reseponse.debugDescription)
+                            }
+                            
+                        })
+
+                    }
+                }
+                
+            }
+            else{
+                if (self.sessionDelegate != nil){
+                    OperationQueue.main.addOperation({
+                        
+                        self.sessionDelegate?.WSError(errorMessage:(error?.localizedDescription)!)
+                    })
+                    
+                }
+            }
+        })
+        dataTask?.resume()
+        
+        
+        
+    }
+    
+    
+}
